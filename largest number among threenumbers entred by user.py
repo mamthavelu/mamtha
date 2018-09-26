5f5151804d3294bd847a8fb7f2a8ede1219f171d
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-09-26 17:23:27 +0530

    Create largest number among threenumbers entred by user.py

---
+++ largest number among threenumbers entred by user.py
@@ -0,0 +1,11 @@
+num1=10
+num2=14
+num3=12
+if(num1>=num2)and(num1>=num3):
+     Largest=num1
+elif(num2>=num1)and(nu8m2>=num3):
+     Largest=num2 
+else:
+    Largest=num3
+print("Larjest no:")
+         
