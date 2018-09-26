4349e80312c1ec596bf5735732dea9469a4ecfd7
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-09-26 17:43:41 +0530

    Update and rename hello world print n times given n to hello world print n times given n.py

---
+++ hello world print n times given n.py
@@ -0,0 +1,4 @@
+for i in range(90):
+    print("hello world")for i in range(90):
+    print("hello world")
+https://github.com/mamthavelu/mamtha/blob/master/hello%20world%20print%20n%20times%20given%20n
