515ebf00f2a9c551f89dd76b030de513285edabc
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-09-27 17:39:06 +0530

    Create number is prime no or not.py

---
+++ number is prime no or not.py
@@ -0,0 +1,17 @@
+number=int(input("enter any number:"))
+if number>1:
+    for i in range(2,number):
+        if(number%i==0):
+            print("number is a not prime:")
+        else:
+            print("number is a prime:")
+    
+    number=int(input("enter any number:"))
+if number>1:
+    for i in range(2,number):
+        if(number%i==0):
+            print("number is a not prime:")
+        else:
+            print("number is a prime:")
+    
+    
