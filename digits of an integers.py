7100ce110befa7f8c0339b8079cd0ceabc37cf4c
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-09-27 16:48:16 +0530

    Create digits of an integers.py

---
+++ digits of an integers.py
@@ -0,0 +1,17 @@
+N=int(input("please Enter any Number:"))
+Count=0
+While(N>0):
+   N=N//10
+   count=count+1:
+print("\n Number of digits in a Given number=%d"%count)
+    
+
+    N=int(input("please Enter any Number:"))
+Count=0
+While(N>0):
+   N=N//10
+   count=count+1:
+print("\n Number of digits in a Given number=%d"%count)
+    
+
+    
