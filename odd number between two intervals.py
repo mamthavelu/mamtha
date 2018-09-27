65ddb71ad79322dd74c8b206581d58692401dd15
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-09-27 17:55:14 +0530

    Lower=int(input("enter the lower limit for range:") Upper=int(input("enter the upper limit for range:")    for i in range(lower,uper+1);:       if(i%2=0)         print(i)

---
+++ odd number between two intervals.py
@@ -0,0 +1,13 @@
+Lower=int(input("enter the lower limit for range:")
+Upper=int(input("enter the upper limit for range:")
+   for i in range(lower,uper+1);:
+      if(i%2=0)Lower=int(input("enter the lower limit for range:")
+Upper=int(input("enter the upper limit for range:")
+   for i in range(lower,uper+1);:
+      if(i%2=0)
+        print(i)
+        print(i)Lower=int(input("enter the lower limit for range:")
+Upper=int(input("enter the upper limit for range:")
+   for i in range(lower,uper+1);:
+      if(i%2=0)
+        print(i)
