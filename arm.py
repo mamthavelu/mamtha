521640792c1520b0d3254af608ffc83377170b53
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-09-28 17:53:11 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -7,22 +7,4 @@ else:
     for i in range(1,n+1):
       factorial=factorial*i:
     print("nois factorial")
-    n=int(input("enter the number"))
-11
-if n<0:
-12
-    print("factorial is negative number")
-13
-elif n==0:
-14
-    print("factorial of0to1")
-15
-else:
-16
-    for i in range(1,n+1):
-17
-      factorial=factorial*i:
-18
-    print("nois factorial")
-19
     
