eca13e4481b5ed53f06523f56ec141a90f3c9fe7
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-09-28 17:02:09 +0530

    Create display  even numbers between two intervals.py

---
+++ display  even numbers between two intervals.py
@@ -0,0 +1,28 @@
+ower=int(input("enter the upper range"))
+upper=int(input("enter the upper range"))
+print("even numbers between",lower,"and",upper,"and:")
+    for num in range(lower,upper+1)ower=int(input("enter the upper range"))
+upper=int(input("enter the upper range"))
+print("even numbers between",lower,"and",upper,"and:")
+    for num in range(lower,upper+1)
+       if(upper%2==0):
+           for i in range(2,num):
+               if(num%i)==0:
+                   break
+               else:
+                   print (num)
+       iower=int(input("enter the upper range"))
+upper=int(input("enter the upper range"))
+print("even numbers between",lower,"and",upper,"and:")
+    for num in range(lower,upper+1)
+       if(upper%2==0):
+           for i in range(2,num):
+               if(num%i)==0:
+                   break
+               else:
+                   print (num)f(upper%2==0):
+           for i in range(2,num):
+               if(num%i)==0:
+                   break
+               else:
+                   print (num)
