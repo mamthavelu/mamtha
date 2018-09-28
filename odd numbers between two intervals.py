d2b17906c91448fdfdfaf0494122e71d90f5f8b7
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-09-28 16:37:47 +0530

    Update odd numbers between two intervals.py

--- odd numbers between two intervals.py
+++ odd numbers between two intervals.py
@@ -2,4 +2,4 @@ lower=int(input("enter the lower for the range:")
 upper=int(input("enter the upper for the range:")
    for i in range(lower,upper+1);
       if(i%2!=0)
-        print(i)mamthavelu Lowe
+        print(i)
