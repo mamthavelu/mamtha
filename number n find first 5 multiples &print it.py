5d018b97a10a41ed9fe451e8c270b1d0807935a9
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-01 17:10:08 +0530

    Create number n find first 5 multiples &print it.py

---
+++ number n find first 5 multiples &print it.py
@@ -0,0 +1,9 @@
+ print/("to find first 5 multiples ")
+       n=int(input("enter the number"))
+     for x in range(1,6):
+         m=n*x
+         print(m) print/("to find first 5 multiples ")
+       n=int(input("enter the number"))
+     for x in range(1,6):
+         m=n*x
+         print(m)
