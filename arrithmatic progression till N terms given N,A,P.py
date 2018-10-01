34e666fb256b3f1c6aa5a1e8f1427c2698304c0a
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-01 17:30:29 +0530

    Update and rename arrithmatic progression till N terms given N,A,P to arrithmatic progression till N terms given N,A,P.py

--- arrithmatic progression till N terms given N,A,P.py
+++ arrithmatic progression till N terms given N,A,P.py
@@ -5,3 +5,4 @@ d=int(input("enter the number"))
 N=int(input("enter the number"))
 print("the",N,"th term of the series is:",Nth_of_AP(a,d,N)) def Nth_of_AP(a,d,N):
     
+https://github.com/mamthavelu/mamtha/blob/master/arrithmatic%20progression%20till%20N%20terms%20given%20N%2CA%2CP
