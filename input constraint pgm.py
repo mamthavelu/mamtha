0a97147255735ff872f5f0af1e5fd057e2735989
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-03 17:20:57 +0530

    Rename input constraint.py to input constraint pgm.py

---
+++ input constraint pgm.py
@@ -0,0 +1,12 @@
+sum=[]
+N=int(input("n"))
+if(1<=N<1000):
+    print("enter your",N,"integer")
+    fori in range(1,n+1):
+        a=int(input("c="))
+        sum.append(a)
+        print(sorted(sum))
+        meadian=sorted(sum)[len(sum)//2]
+        print(median)  
+else:
+    print("invalid")
