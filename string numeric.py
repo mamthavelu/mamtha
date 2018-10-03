2ba472c6e61003ec55c76ef83146423540ce5ea1
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-03 17:29:15 +0530

    Create string numeric.py

---
+++ string numeric.py
@@ -0,0 +1,7 @@
+str='a123'
+#str='123'
+try:
+   i=float(str)
+except(value error,Type error):
+   print("\n Not Numeric')
+print()https://github.com/mamthavelu/mamtha/blob/master/input%20constraint.py
