1aee45698bf52b4044b1edb8c497c69defad1714
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-03 17:39:49 +0530

    Create in.py

---
+++ in.py
@@ -0,0 +1,9 @@
+void main()
+{
+int hr,min,hr1,min1,hr2,min2;
+printf("enter 2 times in hour and minute format");
+scanf("%d%d",&hr1,&min1);
+scanf("%d%d",&hr2,&min2);
+hr=hr2-hr1;
+min=min2-min1;
+printf("
