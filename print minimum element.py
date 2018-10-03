4d9f0497183abb749b26709f46b95642ce5f69b2
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-03 16:48:52 +0530

    Create print minimum element.py

---
+++ print minimum element.py
@@ -0,0 +1,8 @@
+def main():
+    numbers=eval (int("Give mean arryof number:"))
+    smallest=number[0]
+    for i in range(0,Len(numbers),1):
+        if(number[i]<smallest):
+            smallest=numbers[i]
+            print("The smallest number is :",smallest)
+main()
