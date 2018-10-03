2d4aa277a7e458a77633771141f31e0135b4ac79
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-03 16:40:23 +0530

    Update print maximum element.py

--- print maximum element.py
+++ print maximum element.py
@@ -2,14 +2,4 @@ def Largest(arr,n):
     return max(arr)
 arr=[10,325,45,90]
 n=len(arr)
-print(Largest(arr,n))ef Largest(arr,n):
-6
-    return max(arr)
-7
-arr=[10,325,45,90]
-8
-n=len(arr)
-9
 print(Largest(arr,n))
-10
-​
