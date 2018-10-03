602454b6dc422626a657129061769f7de160eefd
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-03 17:35:22 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
-{
+
 int a[10],i,n;
 scanf("%d",&n);
-for(i=0;i<=n;i++)
-{
+for(i=0;i<=n;i++)r
+
 scanf("%d",&a[i]);
-}
+
 for(i=0;i<=n-1;i++)
-{
-printf("%d %d\n",a[i],i);
-}return 0;
-}
+
+print("%d %d\n",a[i],i);
+return 0;
+
