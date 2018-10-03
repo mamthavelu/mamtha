1ea411618b4d28f8e00119ce69fb16a4b45018cc
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-03 16:39:46 +0530

    Update print maximum element.py
    ef Largest(arr,n):
    6
        return max(arr)
    7
    arr=[10,325,45,90]
    8
    n=len(arr)
    9
    print(Largest(arr,n))
    10
    ​

--- print maximum element.py
+++ print maximum element.py
@@ -2,8 +2,14 @@ def Largest(arr,n):
     return max(arr)
 arr=[10,325,45,90]
 n=len(arr)
-print(Largest(arr,n))def Largest(arr,n):
+print(Largest(arr,n))ef Largest(arr,n):
+6
     return max(arr)
+7
 arr=[10,325,45,90]
+8
 n=len(arr)
+9
 print(Largest(arr,n))
+10
+​
