38fb4a149c2ca01aa8d3d8c4439ca087fda673c1
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-04 17:02:55 +0530

    Create 789.py

---
+++ 789.py
@@ -0,0 +1,11 @@
+string=("enter string:")
+char=0
+word=1
+for i in string:
+    char=char+1
+    if(i=='')
+        word=word+1
+    print("numberof words in thestring:")
+    print(word)
+    print("number of characters in the string:")
+    print(char)
