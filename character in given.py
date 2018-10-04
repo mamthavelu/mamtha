642884fcb70654707cd924e03d8e76e44fa949da
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-04 16:56:22 +0530

    Create character in given.py

---
+++ character in given.py
@@ -0,0 +1,9 @@
+print("enter 'o' for exit");
+string=input("enter any string to count character:");
+if(sting=='0'):
+    exit();
+else:
+    char=input("enter a character to count to count from above string:");
+    val=string.count(char);
+    print("Total=",val);
+    
