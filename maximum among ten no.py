950dfc2f74574835758c8f220c604a020ddfec28
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-11 16:32:54 +0530

    Create maximum among ten no.py

---
+++ maximum among ten no.py
@@ -0,0 +1,8 @@
+num=int(input(i in range(0,10)))
+if(num1>=num2)and(nu7m1>=num3):
+    largest=num1
+elif(num2>=num1)and(num2>=num3):
+    largest=num2
+else
+    largest=num3
+print("the largest number between num,",",num2,"and",num3,"is",largest)
