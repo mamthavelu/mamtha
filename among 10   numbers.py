3048acf29451cb4bc46b205627add8dfe42bab47
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2018-10-15 16:46:05 +0530

    Create among 10   numbers.py

---
+++ among 10   numbers.py
@@ -0,0 +1,7 @@
+if(num1>=num2)&(num1>=num3):
+    largest =num1
+    elif(num2>=num1)&(num2>=num3):
+        largest=num2
+        else:
+            largest=num3
+            print("the largest num1,num2,num3")
