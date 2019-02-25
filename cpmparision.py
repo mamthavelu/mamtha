7c1df32e1495c456fff8ef68ee544563e7c2a6d0
mamthavelu <43172402+mamthavelu@users.noreply.github.com>
2019-02-25 13:54:16 +0530

    Create cpmparision.py

---
+++ cpmparision.py
@@ -0,0 +1,10 @@
+str1=input("enter a str1")
+str2=input("enter a str2")
+if(str1<str2):
+     printf("greater string is",str2)
+elif(str1=str2):
+     printf("str1:",str1)
+else:
+     printf("str1:",str1)
+     
+  
